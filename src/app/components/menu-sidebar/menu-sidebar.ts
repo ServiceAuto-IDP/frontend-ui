cdefe61d355f2a7fc3515a318be9e38ada014eec
Savu Paul-Cristian <paul.savuxx@yahoo.ca>
2026-05-04 13:25:35 +0300

    Implemented logout functionality.

--- src/app/components/menu-sidebar/menu-sidebar.ts
+++ src/app/components/menu-sidebar/menu-sidebar.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-menu-sidebar',
@@ -7,4 +8,11 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './menu-sidebar.html',
   styleUrl: './menu-sidebar.css',
 })
-export class MenuSidebar {}
+export class MenuSidebar {
+  constructor(private router: Router, private authService: AuthService) {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
+}
